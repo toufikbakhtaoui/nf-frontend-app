d80287fa996e146815f9ae2375101aa737205559
toufikbakhtaoui <toufik.bakhtaoui@gmail.com>
2021-10-24 00:33:50 +0200

    first commit basic app with snowpack and vuejs

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="App">
+    <header>
+      {{ message }}
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Welcome to the nfl",
+    };
+  },
+};
+</script>
+
+<style>
+.App {
+  text-align: center;
+}
+</style>
