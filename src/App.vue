39ea58859c50c7fe498c0f11d12b859ae619b4fc
toufikbakhtaoui <toufik.bakhtaoui@gmail.com>
2021-10-30 00:17:10 +0200

    setup basic navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="App">
-        <div class="text-center py-4">
-            {{ message }}
+    <div class="App h-screen w-scree">
+        <header class="bg-blue-50">
+            <nav class="container flex justify-evenly py-2">
+                <a>Home</a>
+                <a>Schedule</a>
+                <a>Standings</a>
+                <a>Pantheon</a>
+            </nav>
+        </header>
+        <div class="border-2 text-center bg-gray-100 h-full">
+            <p>content</p>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            message: 'Welcome to the nfl!',
-        }
-    },
-}
+export default {}
 </script>
 
 <style></style>
