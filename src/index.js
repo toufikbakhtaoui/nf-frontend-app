09c7e1a608b43b95cf60561a7ac9489b506f2599
toufikbakhtaoui <toufik.bakhtaoui@gmail.com>
2023-01-01 03:31:03 +0100

    add echarts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import App from '../src/App.vue'
 import router from '../src/router'
 import '../src/main.css'
 
+import ECharts from 'vue-echarts'
+import { use } from 'echarts/core'
+
+import { CanvasRenderer } from 'echarts/renderers'
+import { BarChart } from 'echarts/charts'
+import { GridComponent, TooltipComponent } from 'echarts/components'
+
+use([CanvasRenderer, BarChart, GridComponent, TooltipComponent])
+
 const app = createApp(App)
+app.component('v-chart', ECharts)
 app.use(router)
 app.mount('#app')
